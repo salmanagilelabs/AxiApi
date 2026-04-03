589827f73b1cdd16d83a65dc80e9523eaf0bcd63
salmanagilelabs <salman.s@agile-labs.com>
2026-04-03 16:00:58 +0530

    feat: implement custom favorites

--- DTOs/UserFavouritesRequestDTO.cs
+++ DTOs/UserFavouritesRequestDTO.cs
@@ -5,6 +5,7 @@
        
         public string Username { get; set; } = string.Empty;
         public string CommandText { get; set; } = string.Empty;
+        public string OriginalCommandText { get; set; } = string.Empty; 
         public int? FavOrder { get; set; }
         public string Action { get; set; } = string.Empty;
         public string TargetURL { get; set; } = string.Empty;
--- Models/UserFavourites.cs
+++ Models/UserFavourites.cs
@@ -4,7 +4,8 @@
     {
         public Guid FavouritesId { get; set;  }
         public string Username { get; set; } = string.Empty;
-        public string CommandText { get; set; } = string.Empty; 
+        public string CommandText { get; set; } = string.Empty;
+        public string OriginalCommandText { get; set; } = string.Empty; 
         public int FavOrder { get; set;  }
         public string TargetURL { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; }
